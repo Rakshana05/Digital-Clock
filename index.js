181dfb23fdadfc7b864d4545eb46e8f5a4fae387
Rakshana05 <114346437+Rakshana05@users.noreply.github.com>
2022-12-11 15:06:03 +0530

    Update index.js
    "3" Hours will be viewed as "03" hours, applied for minutes and seconds

--- index.js
+++ index.js
@@ -9,13 +9,19 @@ function updateTime(){
   var min=new Date().getMinutes();
   var sec=new Date().getSeconds();
   
+  h.innerText = hour;
   m.innerText = min;
   s.innerText = sec;
   
   if(hour>12){
     h.innerText = hour - 12;
+    hour = hour-12;
     ampm.innerText = 'PM';
   }
+  
+  if(hour<10){ h.innerText = "0"+ hour.toString();}
+  if(min<10){ m.innerText = "0"+min.toString();}
+  if(sec<10){ s.innerText = "0"+sec.toString();}
   
   setTimeout(updateTime,1000);
 }
